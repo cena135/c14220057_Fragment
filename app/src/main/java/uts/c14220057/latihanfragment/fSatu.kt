6d0cc74cc407c55c3d54ff5c874577cdf742180f
Alexander <jcena1f.e@gmail.com>
2024-10-29 23:40:41 +0700

    fragment satu fix

--- app/src/main/java/uts/c14220057/latihanfragment/fSatu.kt
+++ app/src/main/java/uts/c14220057/latihanfragment/fSatu.kt
@@ -1,25 +1,21 @@
 package uts.c14220057.latihanfragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [fSatu.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fSatu : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var firstButton: Button? = null // Track the first button pressed
+    private var firstButtonNumber: Int? = null // Track the number on the first button pressed
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +29,73 @@ class fSatu : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f_satu, container, false)
+        val view = inflater.inflate(R.layout.fragment_f_satu, container, false)
+
+        // Randomize button numbers
+        randomizeButtonNumbers(view)
+
+        return view
+    }
+
+    private fun randomizeButtonNumbers(view: View) {
+        // Get references to the buttons
+        val buttons = arrayOf(
+            view.findViewById<Button>(R.id.btn1),
+            view.findViewById<Button>(R.id.btn2),
+            view.findViewById<Button>(R.id.btn3),
+            view.findViewById<Button>(R.id.btn4),
+            view.findViewById<Button>(R.id.btn5),
+            view.findViewById<Button>(R.id.btn6),
+            view.findViewById<Button>(R.id.btn7),
+            view.findViewById<Button>(R.id.btn8),
+            view.findViewById<Button>(R.id.btn9),
+            view.findViewById<Button>(R.id.btn10)
+        )
+
+        val numbers = mutableListOf(1, 1, 2, 2, 3, 3, 4, 4, 5, 5)
+        numbers.shuffle()
+
+        buttons.forEachIndexed { index, button ->
+            button.text = ""
+            button.setTextColor(android.graphics.Color.TRANSPARENT)
+
+            button.setOnClickListener {
+                // Show the number and change color
+                button.text = numbers[index].toString()
+                button.setTextColor(android.graphics.Color.BLACK)
+
+                if (firstButton == null) {
+                    // First button pressed
+                    firstButton = button
+                    firstButtonNumber = numbers[index]
+                } else {
+                    // Second button pressed
+                    val secondButton = button
+                    val secondButtonNumber = numbers[index]
+
+                    // Check if numbers match
+                    if (firstButtonNumber == secondButtonNumber) {
+                        // If the same, reset firstButton to null
+                        firstButton = null
+                    } else {
+                        // If different, keep the numbers visible for 1 second
+                        Handler().postDelayed({
+                            // Hide numbers by resetting text and color
+                            firstButton?.text = ""
+                            firstButton?.setTextColor(android.graphics.Color.TRANSPARENT)
+                            secondButton.text = ""
+                            secondButton.setTextColor(android.graphics.Color.TRANSPARENT)
+
+                            // Reset firstButton to null
+                            firstButton = null
+                        }, 1000)
+                    }
+                }
+            }
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fSatu.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             fSatu().apply {
